010396102979b9f5e55ccf61f2072f0844faf61d
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-06-20 15:53:17 -0300

    docker-compose setup

--- tg_bot/bot_core.py
+++ tg_bot/bot_core.py
@@ -1,1 +1,13 @@
-print("Hello")
+from time import sleep
+from datetime import datetime
+
+
+def main():
+    while True:
+        current_time = datetime.now()
+        print(current_time)
+        sleep(300)
+
+
+if __name__ == "__main__":
+    main()
