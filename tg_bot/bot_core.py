106e44844a83c91573bc8589b4fdce5e34a28e2a
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-06-22 11:12:57 -0300

    read from db added

--- tg_bot/bot_core.py
+++ tg_bot/bot_core.py
@@ -1,12 +1,32 @@
 from time import sleep
-from datetime import datetime
+import os
+import psycopg2
 
 
 def main():
-    while True:
-        current_time = datetime.now()
-        print(current_time)
-        sleep(300)
+    conn = psycopg2.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+    )
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM person"
+
+    i = 0
+    while i < 10:
+        i += 1
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        sleep(10)
+
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
